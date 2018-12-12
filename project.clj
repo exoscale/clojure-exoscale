d931e665249a779c756d2f096560b6c536f372a2
Pierre-Yves Ritschard <pyr@spootnik.org>
2018-12-12 10:03:39 +0100

    early 0.1.0 release

--- project.clj
+++ project.clj
@@ -13,5 +13,4 @@
                  [clj-time            "0.15.1"]
                  [aleph               "0.4.6"]
                  [com.jcraft/jsch     "0.1.54"]
-                 [toml                "0.1.3"]
-                 ])
+                 [toml                "0.1.3"]])
