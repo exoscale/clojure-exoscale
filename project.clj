9be9ec2a8438252ffeba05d9a32776d1bc28cd96
David Jacot <david.jacot@gmail.com>
2019-04-03 11:29:18 +0200

    bump codox to 0.10.6

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :plugins [[lein-kibit      "0.1.6"]
             [lein-ancient    "0.6.15"]
             [jonase/eastwood "0.3.3"]
-            [lein-codox      "0.10.5"]]
+            [lein-codox      "0.10.6"]]
   :codox {:source-uri  "https://github.com/exoscale/clojure-exoscale/blob/{version}/{filepath}#L{line}"
           :doc-files   ["doc/intro.md"]
           :metadata    {:doc/format :markdown}}
