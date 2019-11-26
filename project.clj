b0e4349ac2696317db479d1e3fb0932a8e560391
Pierre-Yves Ritschard <pyr@spootnik.org>
2019-11-26 15:19:28 +0100

    project: remove duplicate key

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject exoscale/exoscale "0.1.7-SNAPSHOT"
   :description "All things Exoscale, in Clojure"
   :url "https://github.com/exoscale/clojure-exoscale"
-  :deploy-repositories [["snapshots" :clojars] ["releases" :clojars]]
   :plugins [[lein-kibit      "0.1.6"]
             [lein-ancient    "0.6.15"]
             [jonase/eastwood "0.3.3"]
