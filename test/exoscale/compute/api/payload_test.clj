4b3373180025e40e6f147645d837034a86189e21
Ivan Grishaev <ivan@grishaev.me>
2019-12-11 18:13:58 +0300

    CH7747

---
+++ test/exoscale/compute/api/payload_test.clj
@@ -0,0 +1,27 @@
+(ns exoscale.compute.api.payload-test
+  (:require [exoscale.compute.api.payload :refer [build-payload]]
+            [clojure.test :refer :all]
+            [clj-time.core :as t]))
+
+(def API_KEY "key")
+(def API_SECRET "secret")
+
+(deftest test-payload
+  (with-redefs [t/now (constantly (t/date-time 2019))]
+    (let [payload (build-payload
+                   {:api-key API_KEY
+                    :api-secret API_SECRET
+                    :expiration 3600}
+                   :someApiCall
+                   {:arg1 "test"
+                    :arg2 42})]
+
+      (is (= {:arg1 "test"
+              :arg2 42
+              :command :someApiCall
+              :apiKey "key"
+              :response "json"
+              :signatureVersion "3"
+              :expires "2019-01-01T00:10:00+0000"
+              :signature "O0Dpq8eI8F+UbjqFs0LOhgQXfV0="}
+             payload)))))
