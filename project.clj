0294e638b43e9c06e6d4e16edc76f63675924bd4
Pierre-Yves Ritschard <pyr@spootnik.org>
2019-12-11 16:28:00 +0100

    Version 0.1.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/exoscale "0.1.9-SNAPSHOT"
+(defproject exoscale/exoscale "0.1.9"
   :description "All things Exoscale, in Clojure"
   :url "https://github.com/exoscale/clojure-exoscale"
   :plugins [[lein-kibit      "0.1.6"]
