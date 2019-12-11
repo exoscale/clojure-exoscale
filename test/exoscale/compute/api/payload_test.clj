f51a6f246038e65bd6a1cd2189f90385dc06e890
Ivan Grishaev <ivan@grishaev.me>
2019-12-11 16:24:22 +0300

    udpated

--- test/exoscale/compute/api/payload_test.clj
+++ test/exoscale/compute/api/payload_test.clj
@@ -1,31 +1,10 @@
 (ns exoscale.compute.api.payload-test
-  (:require [exoscale.compute.api.payload :refer [sign build-payload]]
+  (:require [exoscale.compute.api.payload :refer [build-payload]]
             [clojure.test :refer :all]
             [clj-time.core :as t]))
 
 (def API_KEY "key")
 (def API_SECRET "secret")
-
-;; NOTE: the following test has been borrowed from clostack:
-;; https://github.com/exoscale/clostack/blob/master/test/clostack/payload_test.clj
-;; It doesn't work here as we've got changes in the signing code.
-;; What should be done here is, we need to decide which signing
-;; method is better and correct the test.
-
-#_
-(deftest test-sign-cases
-  (testing "sha1 signature case sensitivity"
-    (is (not= (sign {"a" "a" "b" "B"} API_SECRET)
-              (sign {"a" "a" "B" "B"} API_SECRET)))
-    (is (not= (sign {:a "a" :b "B"} API_SECRET)
-              (sign {:a "a" :B "B"} API_SECRET)))
-    (is (not= (sign {:a "a" :b [{:c 1 :d true}]} API_SECRET)
-              (sign {:a "a" :b [{:c 1 :D true}]} API_SECRET))))
-
-  (testing "keys should be uniq"
-    (is (thrown-with-msg? Exception #"Keys should not be duplicated"
-                          (sign {:a "foo" :A "bar"} API_SECRET)))))
-
 
 (deftest test-payload
   (with-redefs [t/now (constantly (t/date-time 2019))]
