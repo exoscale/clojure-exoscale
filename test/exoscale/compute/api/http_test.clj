fdfea0e10f78f2b8234f25ebe53b38ded2cae012
mcorbin <corbin.math@gmail.com>
2020-01-08 16:08:11 +0100

    fixup! fixup! Fix the find-payload function

--- test/exoscale/compute/api/http_test.clj
+++ test/exoscale/compute/api/http_test.clj
@@ -3,18 +3,27 @@
             [exoscale.compute.api.http :as http]))
 
 (deftest find-payload-test
-  (is (= {:id "bar"}
-         (http/find-payload {:network {:id "bar"}})))
-  (is (= [{:id "bar"}]
-         (http/find-payload {:network [{:id "bar"}]})))
-  (is (= {:success true}
-         (http/find-payload {:success true})))
-  (is (= {:id "bar"
-          :name "foo"}
-         (http/find-payload {:id "bar"
-                             :name "foo"})))
-  (is (= {:jobid "dac8bd04-31ff-11ea-ba11-6bef69b2ab5f"}
-         (http/find-payload {:jobid "dac8bd04-31ff-11ea-ba11-6bef69b2ab5f"})))
-  (is (= [{:id "bar"}]
-         (http/find-payload {:network [{:id "bar"}]
-                             :count 1}))))
+  (testing "map with a top level key"
+    (is (= {:id "bar"}
+           (http/find-payload {:network {:id "bar"}}))))
+  (testing "vector with a top level key"
+    (is (= [{:id "bar"}]
+           (http/find-payload {:network [{:id "bar"}]}))))
+  (testing "map with one element"
+    (is (= {:success true}
+           (http/find-payload {:success true}))))
+  (testing "map with multiple elements"
+    (is (= {:id "bar"
+            :name "foo"}
+           (http/find-payload {:id "bar"
+                               :name "foo"}))))
+  (testing "empty map"
+    (is (= {}
+           (http/find-payload {}))))
+  (testing "map containinb a :jobid key"
+    (is (= {:jobid "dac8bd04-31ff-11ea-ba11-6bef69b2ab5f"}
+           (http/find-payload {:jobid "dac8bd04-31ff-11ea-ba11-6bef69b2ab5f"}))))
+  (testing "vector"
+    (is (= [{:id "bar"}]
+           (http/find-payload {:network [{:id "bar"}]
+                               :count 1})))))
