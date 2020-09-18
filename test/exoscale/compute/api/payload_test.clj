729a6b6135fe25094d2c292fa1d35d7f3222aaae
Max Penet <max.penet@exoscale.ch>
2020-09-18 15:51:46 +0200

    Add support for masked arguments/config values (#17)

--- test/exoscale/compute/api/payload_test.clj
+++ test/exoscale/compute/api/payload_test.clj
@@ -1,6 +1,7 @@
 (ns exoscale.compute.api.payload-test
   (:require [exoscale.compute.api.payload :refer [build-payload]]
             [clojure.test :refer :all]
+            [exoscale.cloak :as cloak]
             [clj-time.core :as t]))
 
 (def API_KEY "key")
@@ -24,4 +25,22 @@
               :signatureVersion "3"
               :expires "2019-01-01T00:10:00+0000"
               :signature "O0Dpq8eI8F+UbjqFs0LOhgQXfV0="}
-             payload)))))
+             payload)))
+
+    (let [payload (build-payload
+                   {:api-key (cloak/mask API_KEY)
+                    :api-secret (cloak/mask API_SECRET)
+                    :expiration 3600}
+                   :someApiCall
+                   {:arg1 "test"
+                    :arg2 (cloak/mask 42)})]
+      (is (= {:arg1 "test"
+              :arg2 42
+              :command :someApiCall
+              :apiKey "key"
+              :response "json"
+              :signatureVersion "3"
+              :expires "2019-01-01T00:10:00+0000"
+              :signature "O0Dpq8eI8F+UbjqFs0LOhgQXfV0="}
+             payload)
+          "we can decode cloaked params and config"))))
