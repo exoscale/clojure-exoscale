db2fd9813e9a46ae1a082a5a50946280c1869b19
Max Penet <max.penet@exoscale.ch>
2020-10-30 09:43:01 +0100

    bump dep

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire            "5.8.1"]
                  [clj-time            "0.15.1"]
-                 [exoscale/net-http   "0.1.0-alpha3"]
+                 [exoscale/net-http   "0.1.0-alpha4"]
                  [com.jcraft/jsch     "0.1.54"]
                  [toml                "0.1.3"]
                  [exoscale/cloak      "0.1.1"]]
