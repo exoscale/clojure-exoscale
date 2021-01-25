dfbd9a4c305ecea1adfbf85aa3e4ecc32b3effcb
Max Penet <max.penet@exoscale.ch>
2021-01-25 16:46:24 +0100

    Version 0.2.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/exoscale "0.2.5"
+(defproject exoscale/exoscale "0.2.6-SNAPSHOT"
   :description "All things Exoscale, in Clojure"
   :url "https://github.com/exoscale/clojure-exoscale"
   :plugins [[lein-kibit      "0.1.6"]
