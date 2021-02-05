f67caf47fee50013585620da31371e994217d6d5
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2021-02-05 15:58:47 +0100

    Version 0.2.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/exoscale "0.2.9"
+(defproject exoscale/exoscale "0.2.10-SNAPSHOT"
   :description "All things Exoscale, in Clojure"
   :url "https://github.com/exoscale/clojure-exoscale"
   :plugins [[lein-kibit      "0.1.6"]
