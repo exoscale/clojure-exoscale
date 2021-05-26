68e0d8566ecab58363875c6eb280acd9743054ee
Pierre-Yves Ritschard <pyr@spootnik.org>
2021-05-26 15:20:34 +0200

    Version 0.2.12-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/exoscale "0.2.11"
+(defproject exoscale/exoscale "0.2.12-SNAPSHOT"
   :description "All things Exoscale, in Clojure"
   :url "https://github.com/exoscale/clojure-exoscale"
   :plugins [[lein-kibit      "0.1.6"]
